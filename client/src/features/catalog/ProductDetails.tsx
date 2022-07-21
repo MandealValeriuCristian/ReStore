b051e23c717068bb55cafb8cb3111287316bbd96
Mandeal Cristian <mandealvaleriucristian@gmail.com>
2022-07-21 22:57:20 +0300

    81. Updating the product detail component to add items to cart part 2

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -10,7 +10,7 @@ import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails(){
-    const {basket} = useStoreContext();
+    const {basket, setBasket, removeItem} = useStoreContext();
     const {id} = useParams<{id: string}>();
     const [product, setProduct]= useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
@@ -24,7 +24,28 @@ export default function ProductDetails(){
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
     }, [id, item])
+    function handleInputChange(event: any){
+        if(event.target.value >= 0) {
+        setQuantity(parseInt(event.target.value));
+        }
+    }
 
+    function handleUpdateCart(){
+        setSubmitting(true);
+        if (!item || quantity > item.quantity) {
+            const updatedQuantity = item ? quantity - item.quantity : quantity;
+            agent.Basket.addItem(product?.id!, updatedQuantity)
+                .then(basket => setBasket(basket))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false))
+        } else {
+            const updatedQuantity = item.quantity - quantity;
+            agent.Basket.removeItem(product?.id!, updatedQuantity)
+                .then(() => removeItem(product?.id!, updatedQuantity))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false))
+        }
+    }
     if(loading) return <LoadingComponent message='Loading product...' />
     if(!product) return <NotFound />
     return (
@@ -65,6 +86,7 @@ export default function ProductDetails(){
                <Grid container spacing={2}>
                    <Grid item xs={6}>
                        <TextField 
+                            onChange={handleInputChange}
                             variant='outlined'
                             type='number'
                             label='Quantity in Cart'
@@ -74,6 +96,9 @@ export default function ProductDetails(){
                    </Grid>
                    <Grid item xs={6}>
                        <LoadingButton
+                            disabled={item?.quantity === quantity || !item && quantity === 0}
+                            loading={loading}
+                            onClick={handleUpdateCart}
                             sx={{height: '55px'}}
                             color='primary'
                             size='large'
