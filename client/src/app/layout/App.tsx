1bd6d53a87192c1cfccebfc44a5439c20e1096cf
Mandeal Cristian <mandealvaleriucristian@gmail.com>
2022-08-02 12:23:53 +0300

    146. Adding a private route component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -20,6 +20,7 @@ import { fetchBasketAsync } from "../../features/basket/basketSlice";
 import Register from "../../features/account/Register";
 import Login from "../../features/account/Login";
 import { fetchCurrentUser } from "../../features/account/accountSlice";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -70,7 +71,7 @@ if (loading) return <LoadingComponent message='Initialising app...' />
         <Route path='/contact' component={ContactPage} />
         <Route path='/server-error' component={ServerError} />
         <Route path='/basket' component={BasketPage} />
-        <Route path='/checkout' component={CheckoutPage} />
+        <PrivateRoute path='/checkout' component={CheckoutPage} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route component={NotFound} />
