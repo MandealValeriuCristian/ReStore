a96e8e4b7387ba4de99aa73219e5f93a2fa4d09a
Mandeal Cristian <mandealvaleriucristian@gmail.com>
2022-08-05 13:48:32 +0300

    184. Home page design

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -63,9 +63,10 @@ if (loading) return <LoadingComponent message='Initialising app...' />
       <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
     <CssBaseline />
      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-     <Container>
+     <Route exact path='/' component={HomePage} />
+     <Route path={'/(.+)'} render={() => (
+      <Container sx={{ mt: 4 }}>
         <Switch>
-        <Route exact path='/' component={HomePage} />
         <Route exact path='/catalog' component={Catalog} />
         <Route path='/catalog/:id' component={ProductDetails} />
         <Route path='/about' component={AboutPage} />
@@ -79,7 +80,7 @@ if (loading) return <LoadingComponent message='Initialising app...' />
         <Route component={NotFound} />
         </Switch>
      </Container>
-     
+     )}/>
     </ThemeProvider>
   );
 }
